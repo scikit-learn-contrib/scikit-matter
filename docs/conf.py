af766baff01fc19f95e7aa0d5856c84f237ea739
Luthaf <luthaf@luthaf.fr>
2020-11-12 11:39:09 +0100

    Separate sources and configuration for the doc

--- docs/conf.py
+++ docs/conf.py
@@ -19,10 +19,10 @@ import skcosmo
 # -- Project information -----------------------------------------------------
 
 # The master toctree document.
-master_doc = "index"
+master_doc = "source/index"
 
 project = "sklearn-COSMO"
-author = ", ".join(open("../../contributors.txt"))
+author = ", ".join(open(os.path.join(os.path.abspath(".."), "contributors.txt")))
 copyright = "2020, " + author
 
 # The full version, including alpha/beta/rc tags
