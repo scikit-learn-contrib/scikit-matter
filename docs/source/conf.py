422c02e891e9d0d7bc98399a512a2076b77c44e8
Rose Cersonsky <rosecersonsky@gmail.com>
2020-11-12 11:13:16 +0100

    Manually updating version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, Rose K. Cersonsky, Guillaume Fraux, Sergei Kliavinek, Alexand
 author = "Rose K. Cersonsky, Guillaume Fraux, Sergei Kliavinek, Alexander Goscinski, Benjamin Helfrect, and Michele Ceriotti"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.1"
+release = "0.1.0"
 
 
 # -- General configuration ---------------------------------------------------
