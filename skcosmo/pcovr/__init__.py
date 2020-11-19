ef286bf317c3c7f97bbf568b196201b1c7d6713f
rosecers <rosecersonsky@gmail.com>
2020-11-19 18:15:09 +0100

    Improved pcovr docstring

--- skcosmo/pcovr/__init__.py
+++ skcosmo/pcovr/__init__.py
@@ -1,6 +1,6 @@
 """
-The :mod:`skcosmo.selection` module includes FPS and CUR selection, each
-with the optional PCov-flavor
+The :mod:`skcosmo.pcovr` module includes the two distance
+measures, as defined by Principal Covariates Regression (PCovR)
 """
 
 from .pcovr_distances import pcovr_covariance, pcovr_kernel_distance
