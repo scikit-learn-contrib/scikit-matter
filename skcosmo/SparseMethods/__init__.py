2c9c5e0070e0dff277409e0f6ab1992f2adc876e
hurricane642 <klyavinekss@gmail.com>
2021-01-20 04:35:02 +0300

    Update skcosmo/SparseMethods/__init__.py
    Co-authored-by: rosecers <47536110+rosecers@users.noreply.github.com>

--- skcosmo/SparseMethods/__init__.py
+++ skcosmo/SparseMethods/__init__.py
@@ -4,7 +4,4 @@ The :mod:`skcosmo.SparseMethods` module includes SparseKPCA
 
 from .SparseKPCA import SparseKPCA
 
-# from .SparseKPCovR import SparseKPCovR
-# from .SparseKRR import SparseKRR
-
 __all__ = ["SparseKPCA"]
