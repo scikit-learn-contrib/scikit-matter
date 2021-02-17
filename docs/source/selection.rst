0f33db550b1c555c33d4c943461ab32b8ede8426
Sergei Kliavinek <klyavinekss@gmail.com>
2021-02-17 05:47:16 +0300

    fix versions

---
+++ docs/source/selection.rst
@@ -0,0 +1,65 @@
+Data Sub-Selection
+============================
+
+This module contains two feature and sample selection modules corresponding to CUR selection and Farthest Point Sampling.  Each method can be modified to include aspects of supervised methods using the `mixing` parameter (0 - 1), where `mixing = 1` corresponds to traditional, unsupervised CUR and FPS. These methods are the focus of `Imbalzano [2018] <https://aip.scitation.org/doi/10.1063/1.5024611>`_ and Cersonsky [2021] (forthcoming).
+
+.. _CUR-api:
+
+CUR
+###
+
+
+CUR decomposition begins by approximating a matrix :math:`{\mathbf{X}}` using a subset of columns and rows
+
+.. math::
+    \mathbf{\hat{X}} \approx \mathbf{X}_\mathbf{c} \left(\mathbf{X}_\mathbf{c}^- \mathbf{X} \mathbf{X}_\mathbf{r}^-\right) \mathbf{X}_\mathbf{r}.
+
+These subsets of rows and columns, denoted :math:`\mathbf{X}_\mathbf{r}` and :math:`\mathbf{X}_\mathbf{c}`, respectively, are determined by iterative maximization of a leverage score :math:`\pi`, representative of the relative importance of each column or row.
+In each iteration of CUR, we select the column or row that maximizes :math:`\pi` and orthogonalize the remaining columns or rows.
+These steps are iterated until a sufficient number of features has been selected.
+This iterative approach, albeit comparatively time consuming, is the most deterministic and efficient route in reducing the number of features needed to approximate :math:`\mathbf{X}` when compared to selecting all features in a single iteration based upon the relative :math:`\\pi` importance.
+
+These selection methods can be modified to be semi-supervised by using augmented right or left singular vectors, as shown in [Cersonsky 2021].
+
+.. currentmodule:: skcosmo.feature_selection
+.. autoclass:: FeatureCUR
+    :show-inheritance:
+    :special-members:
+
+    .. automethod:: get_product
+    .. automethod:: orthogonalize
+    .. automethod:: select
+
+.. currentmodule:: skcosmo.sample_selection
+.. autoclass:: SampleCUR
+    :show-inheritance:
+    :special-members:
+
+    .. automethod:: get_product
+    .. automethod:: orthogonalize
+    .. automethod:: select
+
+.. _FPS-api:
+
+Farthest Point-Sampling
+#######################
+
+Farthest Point Sampling is a common selection technique intended to exploit the  diversity of the input space.
+
+In FPS, the selection of the first point is made at random or by a separate metric.
+Each subsequent selection is made to maximize the distance to the previous selections.
+It is common to use the Euclidean distance, however other distance metrics may be employed.
+
+These selection methods can be modified to be semi-supervised by using the PCovR covariance and Gram matrices to compute the distances, as shown in [Cersonsky 2021].
+
+.. currentmodule:: skcosmo.feature_selection
+.. autoclass:: FeatureFPS
+    :show-inheritance:
+
+    .. automethod:: select
+
+.. currentmodule:: skcosmo.sample_selection
+.. autoclass:: SampleFPS
+    :show-inheritance:
+
+    .. automethod:: select
---
+++ skcosmo/utils/orthogonalizers.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains the necessary orthogonalizers for the CUR decomposition
+subselection method.
+Authors: Rose K. Cersonsky
+         Michele Ceriotti
+"""
+
+import numpy as np
+
+
+def X_orthogonalizer(X_proxy, c, tol=1e-12):
+    """
+    Orthogonalizes a feature matrix by the given column. Can be used to
+    orthogonalize by samples by calling `X = X_orthogonalizer(X.T, row_index).T`.
+    :param X_proxy: feature matrix to orthogonalize
+    :type X_proxy: matrix of shape (n x m)
+    :param c: index of the column to orthogonalize by
+    :type c: int, less than m
+    """
+
+    if np.sqrt(np.matmul(X_proxy[:, c], X_proxy[:, c])) < tol:
+        raise ValueError("Cannot orthogonalize by a null vector.")
+
+    v = X_proxy[:, c] / np.sqrt(np.matmul(X_proxy[:, c], X_proxy[:, c]))
+
+    for i in range(X_proxy.shape[1]):
+        X_proxy[:, i] -= v * np.dot(v, X_proxy[:, i])
+
+    return X_proxy
+
+
+def feature_orthogonalizer(idx, X_proxy, Y_proxy, tol=1e-12):
+    """
+    Orthogonalizes two matrices, meant to represent a feature matrix
+    :math:`{\\mathbf{X}}` and a property matrix :math:`{\\mathbf{Y}}`, given
+    the selected features :math:`{c}`
+    Update to :math:`{\\mathbf{X}}`, where :math:`{\\mathbf{X}_{c}}` is a column
+    vector containing the most recently-chosen feature:
+    .. math::
+        \\mathbf{X} \\leftarrow \\mathbf{X} -
+        \\left(\\frac{\\mathbf{X}_{c}\\mathbf{X}{c}^T}
+        {\\lVert\\mathbf{X}_{c}\\rVert^2}\\right)\\mathbf{X}
+    Update to :math:`{\\mathbf{Y}}`, where :math:`{\\mathbf{X}_{\\mathbf{c}}}`
+    is a matrix containing all previously-chosen features:
+    .. math::
+        \\mathbf{Y} \\leftarrow \\mathbf{Y} -
+        \\mathbf{X}_{\\mathbf{c}} \\left(\\mathbf{X}_{\\mathbf{c}}^T
+        \\mathbf{X}_{\\mathbf{c}}\\right)^{-1}
+        \\mathbf{X}_{\\mathbf{c}}^T \\mathbf{Y}
+    :param idx: indices of selected features
+    :type idx: list of int
+    :param X_proxy: feature matrix
+    :type X_proxy: array of shape (n_samples x n_features)
+    :param Y_proxy: property matrix
+    :type Y_proxy: array of shape (n_samples x n_properties)
+    :param tol: cutoff for small eigenvalues to send to np.linalg.pinv
+    :type tol: float
+    """
+    if X_proxy is not None:
+        Aci = X_proxy[:, idx]
+
+        if Y_proxy is not None:
+            v = np.linalg.pinv(np.matmul(Aci.T, Aci), rcond=tol)
+            v = np.matmul(Aci, v)
+            v = np.matmul(v, Aci.T)
+
+            Y_proxy -= np.matmul(v, Y_proxy)
+
+        X_proxy = X_orthogonalizer(X_proxy, idx[-1])
+
+    return X_proxy, Y_proxy
+
+
+def sample_orthogonalizer(idx, X_proxy, Y_proxy, tol=1e-12):
+    """
+    Orthogonalizes two matrices, meant to represent a feature matrix
+    :math:`{\\mathbf{X}}` and a property matrix :math:`{\\mathbf{Y}}`, given
+    the selected samples :math:`{r}`
+    Update to :math:`{\\mathbf{X}}`, where :math:`{\\mathbf{x}_{r}}` is a row
+    vector containing the most recently-chosen sample:
+    .. math::
+        \\mathbf{X} \\leftarrow \\mathbf{X} -
+        \\mathbf{X} \\left(\\frac{\\mathbf{x}_{r}^T \\mathbf{x}_{r}}
+        {\\lVert \\mathbf{x}_{r}\\rVert^2}\\right)
+    Update to :math:`{\\mathbf{Y}}`, where :math:`{\\mathbf{X}_{\\mathbf{r}}}`
+    and :math:`{\\mathbf{Y}_{\\mathbf{r}}}`
+    contain the features and properties of the previously-chosen samples:
+    .. math::
+        \\mathbf{Y} \\leftarrow \\mathbf{Y} -
+        \\mathbf{X} \\left(\\mathbf{X}_{\\mathbf{r}}^T
+        \\mathbf{X}_{\\mathbf{r}}\\right)^{-1}\\mathbf{X}_{\\mathbf{r}}^T
+        \\mathbf{Y}_{\\mathbf{r}}
+    :param idx: indices of selected samples
+    :type idx: list of int
+    :param X_proxy: feature matrix
+    :type X_proxy: array of shape (n_samples x n_features)
+    :param Y_proxy: property matrix
+    :type Y_proxy: array of shape (n_samples x n_properties)
+    :param tol: cutoff for small eigenvalues to send to np.linalg.pinv
+    :type tol: float
+    """
+    if X_proxy is not None:
+        if Y_proxy is not None:
+            Y_proxy -= (
+                X_proxy
+                @ (
+                    np.linalg.pinv(X_proxy[idx].T @ X_proxy[idx], rcond=tol)
+                    @ X_proxy[idx].T
+                )
+                @ Y_proxy[idx]
+            )
+
+        X_proxy = X_orthogonalizer(X_proxy.T, idx[-1]).T
+
+    return X_proxy, Y_proxy
