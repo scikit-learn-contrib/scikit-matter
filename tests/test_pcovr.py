70fbbde67c72da4499ad0bd5ae539f59a57642ae
bhelfrecht <ben.helfrecht@gmail.com>
2021-03-01 12:10:27 +0100

    Make PCovR prediction test more robust to small singular values

--- tests/test_pcovr.py
+++ tests/test_pcovr.py
@@ -46,7 +46,7 @@ class PCovRErrorTest(PCovRBaseTest):
         for space in ["feature", "sample", "auto"]:
             with self.subTest(space=space):
                 pcovr = self.model(
-                    mixing=0.0, n_components=self.X.shape[-1], space=space
+                    mixing=0.0, n_components=1, space=space
                 )
 
                 pcovr.estimator.fit(self.X, self.Y)
