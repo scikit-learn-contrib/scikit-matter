1d62c51bf28be5a210c4f6e0b6a81a5ecfac3239
bhelfrecht <ben.helfrecht@gmail.com>
2021-03-01 12:19:04 +0100

    Formatting change

--- skcosmo/decomposition/pcovr.py
+++ skcosmo/decomposition/pcovr.py
@@ -380,9 +380,7 @@ class PCovR(_BasePCA, LinearModel):
             self.explained_variance_ / self.explained_variance_.sum()
         )
 
-        S_inv = np.diagflat(
-            [1.0 / s if s > self.tol else 0.0 for s in S]
-        )
+        S_inv = np.diagflat([1.0 / s if s > self.tol else 0.0 for s in S])
         self.pxt_ = np.linalg.multi_dot([iCsqrt, Vt.T, np.diagflat(S)])
         self.ptx_ = np.linalg.multi_dot([S_inv, Vt, Csqrt])
         self.pty_ = np.linalg.multi_dot([S_inv, Vt, iCsqrt, X.T, Y])
