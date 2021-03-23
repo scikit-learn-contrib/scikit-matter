b47c4cb37addd1ad71a365d0fe1ef5485cd54a55
Michele Ceriotti <michele.ceriotti@epfl.ch>
2021-03-23 16:58:44 +0100

    Dropping zeros makes thing faster and more robust

--- benchmarks/r1_update.py
+++ benchmarks/r1_update.py
@@ -13,7 +13,7 @@ from skcosmo.utils.roupdate import rank1_update  # noqa
 
 n_samples =  1000
 n_features =  100
-n_select = 10
+n_select = 20
 X = make_low_rank_matrix(n_samples=n_samples, n_features=n_features, effective_rank=100)
 X = StandardScaler().fit_transform(X)
 
@@ -44,27 +44,38 @@ r1_idx = np.zeros(n_select, dtype=int)
 X_current = X_r1.copy()
 vC_current = vC.copy()
 UC_current = UC.copy()
+idx_current = list(range(n_features)) # lazy list to keep track of dropped features
 for i in tqdm(range(1, n_select+1)):
     j1 = np.argmax(UC_current[:, -1] ** 2)
     
     r1_times[i-1] = time.time() - start;
     start = time.time()
-    r1_idx[i-1] = j1
+    r1_idx[i-1] = idx_current[j1]
     print("CHECK ", r1_idx[i-1], cur_idx[i-1])
 
     xc = X_current[:, j1]
     xc = xc / np.sqrt(xc @ xc)
-    X1 = x_orth(X_current, c=j1)
-
-    C1 = X1.T @ X1
-    vC1, UC1 = np.linalg.eigh(C1)
+    
 
     v = X_current.T @ xc
     lam, Q = rank1_update(-vC_current[::-1], UC_current[:, ::-1].T @ v)
     vC_current = -1.0 * np.asarray(lam)[::-1]
     UC_current = (UC_current[:, ::-1] @ Q)[:, ::-1]
+    X_current = x_orth(X_current, c=j1)
+    
+    # drops zero eigenvalues and zero rows
+    X_current = np.hstack([X_current[:,:j1], X_current[:,j1+1:]])
+    vC_current = vC_current[1:]
+    UC_current = np.vstack([UC_current[:j1,1:], UC_current[j1+1:,1:]])
+    idx_current.pop(j1)
 
-    print(max(vC1), max(vC_current))
+    #C1 = X_current.T @ X_current
+    #vC1, UC1 = np.linalg.eigh(C1)
+    #print(max(vC1), max(vC_current))
+
+
+print("CUR sel: ", cur_idx)
+print("CUR r1:  ", r1_idx)
 
 plt.semilogy(cur_times, 'b-')
 plt.semilogy(r1_times, 'r-')
