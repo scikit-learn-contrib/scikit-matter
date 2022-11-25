a06b94d2864ef52c291679ba0d39243a0afef340
serfg <sergei.pozdnyakov@gmail.com>
2022-11-25 17:03:49 +0100

    Adding nice dataset (#143)
    * adding nice data files
    
    * teaching skcosmo to read nice dataset
    
    * tests for nice dataset
    
    * fix typo
    
    * fix dataset name in the load function
    
    * placing description to the right place
    
    * formating
    
    * fixing rst syntax
    
    * sorting imports
    
    * black formating
    
    * black formating
    
    * explaining nice acronym

--- skcosmo/datasets/__init__.py
+++ skcosmo/datasets/__init__.py
@@ -1,6 +1,7 @@
 from ._base import (
     load_csd_1000r,
     load_degenerate_CH4_manifold,
+    load_nice_dataset,
 )
 
-__all__ = ["load_degenerate_CH4_manifold", "load_csd_1000r"]
+__all__ = ["load_degenerate_CH4_manifold", "load_csd_1000r", "load_nice_dataset"]
