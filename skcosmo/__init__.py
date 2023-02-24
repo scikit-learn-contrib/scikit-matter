5c0c441615cb7dbe2adeed944369632d98682808
alexgo <alex.goscinski@posteo.de>
2023-02-24 18:29:35 +0100

    add links to renamed skmatter repo

--- skcosmo/__init__.py
+++ skcosmo/__init__.py
@@ -5,5 +5,5 @@ import logging
 LOGGER = logging.getLogger(__name__)
 
 LOGGER.warn(
-    "DeprecationWarning: This package has been renamed to scikit-matter (skmatter). This package will no longer be maintained and updated. Please install the new package using `pip install skmatter`"
+    "DeprecationWarning: This package has been renamed to scikit-matter (skmatter https://github.com/lab-cosmo/scikit-matter). This package will no longer be maintained and updated. Please install the new package using `pip install skmatter`"
 )
