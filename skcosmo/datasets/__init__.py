cb1c07cafc8c2057dcca65a7e225a3fe01130c27
Rose K. Cersonsky <47536110+rosecers@users.noreply.github.com>
2023-02-24 08:45:02 -0600

    Add who dataset, examples and tests
    Adding a publically-available example for the who dataset #149

--- skcosmo/datasets/__init__.py
+++ skcosmo/datasets/__init__.py
@@ -2,6 +2,12 @@ from ._base import (
     load_csd_1000r,
     load_degenerate_CH4_manifold,
     load_nice_dataset,
+    load_who_dataset,
 )
 
-__all__ = ["load_degenerate_CH4_manifold", "load_csd_1000r", "load_nice_dataset"]
+__all__ = [
+    "load_degenerate_CH4_manifold",
+    "load_csd_1000r",
+    "load_nice_dataset",
+    "load_who_dataset",
+]
