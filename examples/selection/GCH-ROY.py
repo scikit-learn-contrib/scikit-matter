ca890f7f8e7d6bacb84186cfda98d4b6a2c6ff4d
Michele Ceriotti <michele.ceriotti@gmail.com>
2023-04-07 00:51:46 +0200

    Cleaned up

--- examples/selection/GCH-ROY.py
+++ examples/selection/GCH-ROY.py
@@ -20,7 +20,7 @@ from skmatter.datasets import load_roy_dataset
 from skmatter.sample_selection import DirectionalConvexHull
 
 
-######################################################################
+# %%
 # Loads the structures (that also contain properties in the ``info``
 # field)
 #
@@ -36,7 +36,7 @@ iknown = np.where(structype == "known")[0]
 iothers = np.where(structype != "known")[0]
 
 
-######################################################################
+# %%
 # Energy-density hull
 # -------------------
 #
@@ -48,7 +48,7 @@ dch_builder = DirectionalConvexHull(low_dim_idx=[0])
 dch_builder.fit(density.reshape(-1, 1), energy)
 
 
-######################################################################
+# %%
 # We can get the indices of the selection, and compute the distance from
 # the hull
 #
@@ -57,7 +57,7 @@ sel = dch_builder.selected_idx_
 dch_dist = dch_builder.score_samples(density.reshape(-1, 1), energy)
 
 
-######################################################################
+# %%
 # Structures on the hull are stable with respect to synthesis at constant
 # molar volume. Any other structure would lower the energy by decomposing
 # into a mixture of the two nearest structures along the hull. Given that
@@ -81,33 +81,35 @@ print(
 print(f"Mean hull energy for 'other' structures {dch_dist[iothers].mean()} kJ/mol")
 
 
-######################################################################
+# %%
 # You can also visualize the hull with ``chemiscope``.
 # This runs only in a notebook, and
 # requires having the ``chemiscope`` package installed.
 #
-# .. code:: python
-#
-#    chemiscope.show(
-#        structures,
-#        dict(
-#            energy=energy, density=density,
-#            hull_energy=dch_dist, structure_type=structype
-#        ),
-#        settings={
-#            "map": {
-#                "x": {"property": "density"},
-#                "y": {"property": "energy"},
-#                "color": {"property": "hull_energy"},
-#                "symbol": "structure_type",
-#                "size": {"factor": 35},
-#            },
-#            "structure": [{"unitCell": True, "supercell": {"0": 2, "1": 2, "2": 2}}],
-#        },
-#    )
+
+"""
+import chemiscope
+chemiscope.show(
+    structures,
+    dict(
+        energy=energy, density=density,
+        hull_energy=dch_dist, structure_type=structype
+    ),
+    settings={
+        "map": {
+               "x": {"property": "density"},
+               "y": {"property": "energy"},
+               "color": {"property": "hull_energy"},
+               "symbol": "structure_type",
+               "size": {"factor": 35},
+        },
+        "structure": [{"unitCell": True, "supercell": {"0": 2, "1": 2, "2": 2}}],
+    },
+)
+"""
 
 
-######################################################################
+# %%
 # Generalized Convex Hull
 # -----------------------
 #
@@ -122,38 +124,40 @@ print(f"Mean hull energy for 'other' structures {dch_dist[iothers].mean()} kJ/mo
 #
 
 
+# %%#
 # A first step is to computes suitable ML descriptors. Here we have used
 # ``rascaline`` to evaluate average SOAP features for the structures. We
 # will load pre-computed features to reduce the dependencies of these
 # examples on external packages, but you can use this as a stub to apply
 # this analysis to other chemical systems
 #
-# .. code:: python
-#
-#    from rascaline import SoapPowerSpectrum
-#    from equistore import mean_over_samples
-#    hypers = {
-#        "cutoff": 4,
-#        "max_radial": 6,
-#        "max_angular": 4,
-#        "atomic_gaussian_width": 0.7,
-#        "cutoff_function": {"ShiftedCosine": {"width": 0.5}},
-#        "radial_basis": {"Gto": {"accuracy": 1e-6}},
-#        "center_atom_weight": 1.0
-#    }
-#    calculator = SoapPowerSpectrum(**hypers)
-#    rho2i = calculator.compute(structures)
-#    rho2i=rho2i.keys_to_samples(['species_center']).keys_to_properties(
-#                       ['species_neighbor_1', 'species_neighbor_2'])
-#    rho2i_structure = mean_over_samples(rho2i,
-#                            sample_names=["center", "species_center"])
-#    np.savez("roy_features.npz", feats=rho2i_structure.block(0).values)
-#
+
+"""
+from rascaline import SoapPowerSpectrum
+from equistore import mean_over_samples
+hypers = {
+    "cutoff": 4,
+    "max_radial": 6,
+    "max_angular": 4,
+    "atomic_gaussian_width": 0.7,
+    "cutoff_function": {"ShiftedCosine": {"width": 0.5}},
+    "radial_basis": {"Gto": {"accuracy": 1e-6}},
+    "center_atom_weight": 1.0
+}
+calculator = SoapPowerSpectrum(**hypers)
+rho2i = calculator.compute(structures)
+rho2i=rho2i.keys_to_samples(['species_center']).keys_to_properties(
+                   ['species_neighbor_1', 'species_neighbor_2'])
+rho2i_structure = mean_over_samples(rho2i,
+                        sample_names=["center", "species_center"])
+np.savez("roy_features.npz", feats=rho2i_structure.block(0).values)
+"""
+
 
 features = roy_data["features"]
 
 
-######################################################################
+# %%
 # Computes PCA projection to generate low-dimensional descriptors that
 # reflect structural diversity. Any other dimensionality reduction scheme
 # could be used in a similar fashion.
@@ -170,7 +174,7 @@ cbar = fig.colorbar(scatter, ax=ax)
 cbar.set_label("energy / kJ/mol")
 
 
-######################################################################
+# %%
 # Builds a convex hull on the first two PCA features
 #
 
@@ -180,7 +184,7 @@ sel = dch_builder.selected_idx_
 dch_dist = dch_builder.score_samples(pca_features, energy)
 
 
-######################################################################
+# %%
 # Generates a 3D Plot
 #
 
@@ -195,7 +199,7 @@ ax.set_zlabel("energy / kJ/mol\n \n", labelpad=11)
 ax.view_init(25, 110)
 
 
-######################################################################
+# %%
 # The GCH construction improves the separation between the hull energies
 # of “known” and hypothetical polymorphs (compare with the density-energy
 # values above)
@@ -207,37 +211,39 @@ print(
 print(f"Mean hull energy for 'other' structures {dch_dist[iothers].mean()} kJ/mol")
 
 
-######################################################################
+# %%
 # Visualize in ``chemiscope``. This runs only in a notebook, and
 # requires having the ``chemiscope`` package installed.
 #
-# .. code:: python
-#
-#    for i, f in enumerate(structures):
-#        for j in range(len(pca_features[i])):
-#            f.info["pca_" + str(j + 1)] = pca_features[i, j]
-#    structure_properties = chemiscope.extract_properties(structures)
-#    structure_properties.update({"per_atom_energy": energy, "hull_energy": dch_dist})
-#    chemiscope.show(
-#        frames=structures,
-#        properties=structure_properties,
-#        settings={
-#            "map": {
-#                "x": {"property": "pca_1"},
-#                "y": {"property": "pca_2"},
-#                "z": {"property": "energy"},
-#                "symbol": "type",
-#                "symbol": "type",
-#                "color": {"property": "hull_energy"},
-#                "size": {"factor": 35, "mode": "linear",
-#                         "property": "", "reverse": True},
-#            },
-#            "structure": [
-#                {
-#                    "bonds": True,
-#                    "unitCell": True,
-#                    "keepOrientation": True,
-#                }
-#            ],
-#        },
-#    )
+
+"""
+import chemiscope
+for i, f in enumerate(structures):
+    for j in range(len(pca_features[i])):
+        f.info["pca_" + str(j + 1)] = pca_features[i, j]
+structure_properties = chemiscope.extract_properties(structures)
+structure_properties.update({"per_atom_energy": energy, "hull_energy": dch_dist})
+chemiscope.show(
+    frames=structures,
+    properties=structure_properties,
+    settings={
+        "map": {
+            "x": {"property": "pca_1"},
+            "y": {"property": "pca_2"},
+            "z": {"property": "energy"},
+            "symbol": "type",
+            "symbol": "type",
+            "color": {"property": "hull_energy"},
+            "size": {"factor": 35, "mode": "linear",
+                     "property": "", "reverse": True},
+        },
+        "structure": [
+            {
+               "bonds": True,
+               "unitCell": True,
+               "keepOrientation": True,
+            }
+        ],
+    },
+)
+"""
