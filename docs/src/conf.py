3d9f92386227de5177e3fea9d6d4f1ff2192c96f
Philip Loche <ploche@physik.fu-berlin.de>
2023-04-17 16:09:07 +0200

    Show link to source code in docs

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -44,6 +44,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
     "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
     "sphinx_gallery.gen_gallery",
     "sphinx_toggleprompt",
 ]
