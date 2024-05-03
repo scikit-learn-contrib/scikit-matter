9cb3f08a845d755be93641adf285f207ac32dd66
Qianjun Xu <92628709+GardevoirX@users.noreply.github.com>
2024-05-03 15:45:03 +0200

    Update src/skmatter/clustering/__init__.py
    Co-authored-by: Philip Loche <philip.loche@posteo.de>

--- src/skmatter/clustering/__init__.py
+++ src/skmatter/clustering/__init__.py
@@ -1,6 +1,6 @@
 """
 This module implements the quick shift clustering algorithm, which is used in
-probabilistic analysis of molecular motifs (PAMM). See https://doi.org/10.1063/1.4900655
+probabilistic analysis of molecular motifs (PAMM). See `Gasparotto and Ceriotti <https://doi.org/10.1063/1.4900655>`_
 for more details."""
 
 from ._quick_shift import QuickShift
