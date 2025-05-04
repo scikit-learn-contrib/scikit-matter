376462d8325313f928e368d09ea014053f5bff51
cajchristian <114787994+cajchristian@users.noreply.github.com>
2025-05-04 14:41:56 -0500

    Adding test for incorrect score threshold type

--- tests/test_greedy_selector.py
+++ tests/test_greedy_selector.py
@@ -49,6 +49,14 @@ class TestGreedy(unittest.TestCase):
             "You cannot specify both `score_threshold` and `full=True`.",
         )
 
+    def test_bad_score_threshold_type(self):
+        with self.assertRaises(ValueError) as cm:
+            _ = GreedyTester(score_threshold_type = "invalid").fit(self.X)
+        self.assertEqual(
+            str(cm.exception),
+            "invalid score_threshold_type, expected one of 'relative' or 'absolute'"
+        )
+
     def test_bad_warm_start(self):
         selector = GreedyTester()
         with self.assertRaises(ValueError) as cm:
