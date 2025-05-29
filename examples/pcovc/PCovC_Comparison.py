2c77b00b70b65904164e9d368d07b3f6bc5d6c39
cajchristian <114787994+cajchristian@users.noreply.github.com>
2025-05-29 17:37:11 -0500

    Touching up pcovc vs pca example

--- examples/pcovc/PCovC_Comparison.py
+++ examples/pcovc/PCovC_Comparison.py
@@ -45,10 +45,10 @@ pca = PCA(n_components=2)
 pca.fit(X_scaled, y)
 T_pca = pca.transform(X_scaled)
 
-fig, axis = plt.subplots()
-scatter = axis.scatter(T_pca[:, 0], T_pca[:, 1], c=y)
-axis.set(xlabel="PC$_1$", ylabel="PC$_2$")
-axis.legend(
+fig, ax = plt.subplots()
+scatter = ax.scatter(T_pca[:, 0], T_pca[:, 1], c=y)
+ax.set(xlabel="PC$_1$", ylabel="PC$_2$")
+ax.legend(
     scatter.legend_elements()[0][::-1],
     load_breast_cancer().target_names[::-1],
     loc="upper right",
@@ -66,9 +66,9 @@ lda.fit(X_scaled, y)
 
 T_lda = lda.transform(X_scaled)
 
-fig, axis = plt.subplots()
-axis.scatter(-T_lda[:], np.zeros(len(T_lda[:])), c=y)
-axis.set(xlabel="LD$_1$", ylabel="LD$_2$")
+fig, ax = plt.subplots()
+ax.scatter(T_lda[:], np.zeros(len(T_lda[:])), c=y)
+ax.set(xlabel="LDA$_1$", ylabel="LDA$_2$")
 
 # %%
 #
@@ -91,21 +91,20 @@ pcovc.fit(X_scaled, y)
 
 T_pcovc = pcovc.transform(X_scaled)
 
-fig, axis = plt.subplots()
-axis.scatter(T_pcovc[:, 0], T_pcovc[:, 1], c=y)
-axis.set(xlabel="PCov$_1$", ylabel="PCov$_2$")
+fig, ax = plt.subplots()
+ax.scatter(T_pcovc[:, 0], T_pcovc[:, 1], c=y)
+ax.set(xlabel="PCov$_1$", ylabel="PCov$_2$")
 
 # %%
 #
 # A side-by-side comparison of the
-# three techniques (PCA, LDA, and PCovC):
+# three maps (PCA, LDA, and PCovC):
 
-n_models = 3
-
-fig, axes = plt.subplots(1, n_models, figsize=(6 * n_models, 5))
-axes[0].scatter(T_pca[:, 0], T_pca[:, 1], c=y)
-axes[0].set_title("PCA")
-axes[1].scatter(-T_lda[:], np.zeros(len(T_lda[:])), c=y)
-axes[1].set_title("LDA")
-axes[2].scatter(T_pcovc[:, 0], T_pcovc[:, 1], c=y)
-axes[2].set_title("PCovC")
+fig, axs = plt.subplots(1, 3, figsize=(18, 5))
+axs[0].scatter(T_pca[:, 0], T_pca[:, 1], c=y)
+axs[0].set_title("PCA")
+axs[1].scatter(T_lda, np.zeros(len(T_lda)), c=y)
+axs[1].set_title("LDA")
+axs[2].scatter(T_pcovc[:, 0], T_pcovc[:, 1], c=y)
+axs[2].set_title("PCovC")
+plt.show()
