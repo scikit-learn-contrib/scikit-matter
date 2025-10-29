9f6d88c89cb8a4d120c5f96dc7b99b3c0f45f1c2
Sofiia Chorna <chernaya.sofiia@gmail.com>
2025-10-29 12:50:24 +0100

    Hightlight more Nones

--- src/skmatter/metrics/_reconstruction_measures.py
+++ src/skmatter/metrics/_reconstruction_measures.py
@@ -110,11 +110,11 @@ def global_reconstruction_error(
         X use input shape (samples, features). For sample reconstruction of Y using X
         use input shape (features, samples).
     train_idx : numpy.ndarray, dtype=int, default=None
-        Array of indices used for training. If None, the complement of the ``test_idx``
-        is used. If ``train_size`` is also ``None``, 2-fold split is taken.
+        Array of indices used for training. If ``None``, the complement of the
+        ``test_idx`` is used. If ``train_size`` is also ``None``, 2-fold split is taken.
     test_idx : numpy.ndarray, dtype=int, default=None
-        Array of indices used for testing. If None, the complement of the ``train_idx``
-        is used. If ``test_size`` is also None, 2-fold split is taken.
+        Array of indices used for testing. If ``None``, the complement of the
+        ``train_idx`` is used. If ``test_size`` is also ``None``, 2-fold split is taken.
     scaler : object implementing fit/transform, default=``StandardFlexibleScaler``
         Scales X and Y before computing the reconstruction measure. The default value
         scales the features such that the reconstruction measure on the training set is
@@ -181,11 +181,11 @@ def pointwise_global_reconstruction_distortion(
         For feature reconstruction of Y using X use input shape (samples, features).
         For sample reconstruction of Y using X use input shape (features, samples).
     train_idx : numpy.ndarray, dtype=int, default=None
-        Array of indices used for training. If None, the complement of the ``test_idx``
-        is used. If ``train_size`` is also None, 2-fold split is taken.
+        Array of indices used for training. If ``None``, the complement of the
+        ``test_idx`` is used. If ``train_size`` is also ``None``, 2-fold split is taken.
     test_idx : numpy.ndarray, dtype=int, default=None
-        Array of indices used for testing. If None, the complement of the ``train_idx``
-        is used. If ``test_size`` is also None, 2-fold split is taken.
+        Array of indices used for testing. If ``None``, the complement of the
+        ``train_idx`` is used. If ``test_size`` is also ``None``, 2-fold split is taken.
     scaler : object implementing fit/transform, default=``StandardFlexibleScaler``
         Scales X and Y before computing the reconstruction measure. The default value
         scales the features such that the reconstruction measure on the training set is
@@ -254,10 +254,10 @@ def global_reconstruction_distortion(
         For sample reconstruction of Y using X use input shape (features, samples).
     train_idx : numpy.ndarray, dtype=int, default=None
         Array of indices used for training. If ``None``, the complement of the
-        ``test_idx`` is used. If ``train_size`` is also None, 2-fold split is taken.
+        ``test_idx`` is used. If ``train_size`` is also ``None``, 2-fold split is taken.
     test_idx : numpy.ndarray, dtype=int, default=None
         Array of indices used for testing. If ``None``, the complement of the
-        ``train_idx`` is used. If ``test_size`` is also None, 2-fold split is taken.
+        ``train_idx`` is used. If ``test_size`` is also ``None``, 2-fold split is taken.
     scaler : object implementing fit/transform, default=``StandardFlexibleScaler``
         Scales X and Y before computing the reconstruction measure. The default value
         scales the features such that the reconstruction measure on the training set is
@@ -333,11 +333,11 @@ def pointwise_local_reconstruction_error(
         Number of neighbour points used to compute the local reconstruction weight for
         each sample/point.
     train_idx : numpy.ndarray, dtype=int, default=None
-        Array of indices used for training. If None, the complement of the ``test_idx``
-        is used. If ``train_size`` is also ``None``, 2-fold split is taken.
+        Array of indices used for training. If ``None``, the complement of the
+        ``test_idx`` is used. If ``train_size`` is also ``None``, 2-fold split is taken.
     test_idx : numpy.ndarray, dtype=int, default=None
         Array of indices used for testing. If ``None``, the complement of the
-        ``train_idx`` is used. If ``test_size`` is also None, 2-fold split is taken.
+        ``train_idx`` is used. If ``test_size`` is also ``None``, 2-fold split is taken.
     scaler : object implementing fit/transform, default=``StandardFlexibleScaler``
         Scales X and Y before computing the reconstruction measure. The default value
         scales the features such that the reconstruction measure on the training set is
@@ -444,11 +444,11 @@ def local_reconstruction_error(
         Number of neighbour points used to compute the local reconstruction weight for
         each sample/point.
     train_idx : numpy.ndarray, dtype=int, default=None
-        Array of indices used for training. If None, the complement of the ``test_idx``
-        is used. If ``train_size`` is also ``None``, 2-fold split is taken.
+        Array of indices used for training. If ``None``, the complement of the
+        ``test_idx`` is used. If ``train_size`` is also ``None``, 2-fold split is taken.
     test_idx : numpy.ndarray, dtype=int, default=None
-        Array of indices used for testing. If None, the complement of the ``train_idx``
-        is used. If ``test_size`` is also None, 2-fold split is taken.
+        Array of indices used for testing. If ``None``, the complement of the
+        ``train_idx`` is used. If ``test_size`` is also ``None``, 2-fold split is taken.
     scaler : object implementing fit/transform, default=``StandardFlexibleScaler``
         Scales X and Y before computing the reconstruction measure. The default value
         scales the features such that the reconstruction measure on the training set is
